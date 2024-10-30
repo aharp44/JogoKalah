5b5efcaf96c0db17e2d64ed1d72e87f38c336c84
aharp44 <lucaskhayashi@gmail.com>
2024-10-30 17:33:17 -0300

    aa

--- src/Fazendeiro.java
+++ src/Fazendeiro.java
@@ -14,14 +14,14 @@ public class Fazendeiro
         int cavaAtual = cavaEscolhida;
         // Distribui 1 semente em cada cava seguinte ateh o numero de sementes acabar
         for (int i=0; i < inicial.getQntSementes(); i++){
-            cavaAtual++;             // Move posicao 1 pra frente
+            cavaAtual++;                    // Move posicao da cavaAtual 1 pra frente
             if (cavaAtual > 11){
-                cavaAtual = 0;       // Move posicao de volta pro 0
+                cavaAtual = 0;              // Se necessario, Move posicao da cavaAtual de volta pro 0
             }
-            cavasSimples.get(cavaAtual).addSementes(1);
+            cavasSimples.get(cavaAtual).addSementes(1);     // Adiciona 1 semente a cava atual
         }
-        inicial.removerTdsSementes();   // Remove tds as sementes da cava escolhida
-        return cavasSimples;            // Retorna todo o array cavas novo
+        inicial.removerTdsSementes();       // Remove tds as sementes da cava escolhida
+        return cavasSimples;                // Retorna todo o array de cavas novo
     }
 
 
